7f2368ad43e5de1ccfb64d9f107fb4bee9843243
Pawan Kumar <pawan.kumar@psagtechnologies.com>
2023-03-15 13:20:13 +0530

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,11 +1,46 @@
 'use strict';
 //DOM - Document Object Model
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number';
+//get data from HTML and manipulate it - using class name
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number';
 
-document.querySelector('.score').textContent = 25;
-document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 25;
+// document.querySelector('.number').textContent = 13;
 
-document.querySelector('.guess').value = 23;
+//document.querySelector('.guess').value = 23;
 
-console.log(document.querySelector('.guess').value);
+//math function in javascript
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = secretNumber;
+let score = document.querySelector('.score').textContent;
+console.log(secretNumber);
+//hanlde button click
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number !';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent =
+      'Congratulation Correct Number !';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'To High !';
+      score--;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost the game !';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'To low !';
+      score--;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost the game !';
+    }
+  }
+  document.querySelector('.score').textContent = score;
+});
+
+// document.querySelector('.guess').addEventListener('change', function () {
+//   console.log(document.querySelector('.guess').value);
+// });
