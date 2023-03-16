f9e1e0ffc0502886a1369041aa95baa951ccd075
Pawan Kumar <pawan.kumar@psagtechnologies.com>
2023-03-16 17:04:42 +0530

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,35 +12,83 @@
 //math function in javascript
 const secretNumber = Math.trunc(Math.random() * 20 + 1);
 document.querySelector('.number').textContent = secretNumber;
-let score = document.querySelector('.score').textContent;
+let score = 20;
+let highscore = 0;
 console.log(secretNumber);
+
+//display message
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 //hanlde button click
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number !';
+    displayMessage('No Number !');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent =
-      'Congratulation Correct Number !';
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'To High !';
-      score--;
-    } else {
-      document.querySelector('.message').textContent = 'You Lost the game !';
+    displayMessage('Congratulation Correct Number !');
+    //Selet Element change body color
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
-  } else if (guess < secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'To low !';
+      displayMessage(guess > secretNumber ? 'To High !' : 'To Low !');
       score--;
+      document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You Lost the game !';
+      displayMessage('You Lost the game !');
     }
   }
-  document.querySelector('.score').textContent = score;
+
+  //   else if (guess > secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = 'To High !';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = 'You Lost the game !';
+  //     }
+  //   } else if (guess < secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = 'To low !';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = 'You Lost the game !';
+  //     }
+  //   }
 });
 
 // document.querySelector('.guess').addEventListener('change', function () {
 //   console.log(document.querySelector('.guess').value);
 // });
+
+/*
+Coding Challenge #1
+Implement a game rest functionality, so that the player can make a new guess!
+Your tasks:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the 'score' and
+'secretNumber' variables
+3. Restore the initial conditions of the message, number, score and guess input
+fields
+4. Also restore the original background color (#222) and number width (15rem)
+GOOD LUCK 😀
+*/
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.score').textContent = 20;
+
+  const secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector('.number').textContent = secretNumber;
+  displayMessage('Start guessing...');
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
