65bf2fc523698068c28bd65322f57f5756adc0cc
Pawan Kumar <pawan.kumar@psagtechnologies.com>
2023-03-27 09:41:29 +0530

    Update script.js

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,37 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseWindow = document.querySelector('.close-modal');
+const btsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btsOpenModal);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btsOpenModal.length; i++) {
+  console.log();
+  // btsOpenModal[i].addEventListener('click', function () {
+  //   console.log('Button Clicked');
+  //   modal.classList.remove('hidden');
+  //   overlay.classList.remove('hidden');
+  // })
+  btsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseWindow.addEventListener('click', closeModal);
+// btnCloseWindow.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  console.log(event);
+});
