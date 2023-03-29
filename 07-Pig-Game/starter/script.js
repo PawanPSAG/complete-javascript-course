0b83e9c268ea7020d3986c3fe217724e593f56c8
Pawan Kumar <pawan.kumar@psagtechnologies.com>
2023-03-29 12:03:22 +0530

    Update script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 //select element using id
 const diceElement = document.querySelector('.dice');
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.getElementById('score--1');
 const btnNew = document.querySelector('.btn--new');
@@ -32,9 +34,17 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //add dice to current score
     currentScore += dice;
-    document.getElementById();
-    current0Element.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    //current0Element.textContent = currentScore;
   } else {
     //switch player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
   }
 });
+
+btnHold.addEventListener('click', function () {});
